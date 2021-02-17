a28acb21d230e652aabc29188de3d88b76652ab1
mmoshihud <63121629+mmoshihud@users.noreply.github.com>
2021-02-17 13:10:13 +0600

    fix a hack issue

--- LabTask1/registration.php
+++ LabTask1/registration.php
@@ -2,33 +2,39 @@
 <html>
 
 <head>
-<title>Registration Form</title>
+  <title>Registration Form</title>
 </head>
 
 <body>
 
-<h1>Registration</h1>
+  <?php
+  echo "You must enter name";
 
-<form>
-  <label for="fname">First name:</label>
-  <input type="text" id="fname" name="fname"><br><br>
-  <label for="email">Email:</label>
-  <input type="text" id="email" name="email"><br><br>
-  <label for="username">User Name:</label>
-  <input type="text" id="username" name="username"><br><br>
-  <label for="password">Password:</label>
-  <input type="text" id="password" name="password"><br><br>
-  <label for="gender">Gender:</label>
-  <input type="radio" id="male" name="gender" value="male">
-  <label for="male">Male</label>
-  <input type="radio" id="female" name="gender" value="female">
-  <label for="female">Female</label>
-  <input type="radio" id="other" name="gender" value="other">
-  <label for="other">Other</label><br><br>
+  ?>
 
-  <input type="submit" value="Submit">
-  <input type="reset" value="Reset">
-</form>
+  <h1>Registration</h1>
+
+  <form>
+    <label for="fname">First name:</label>
+    <input type="text" id="fname" name="fname"><br><br>
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email"><br><br>
+    <label for="username">User Name:</label>
+    <input type="text" id="username" name="username"><br><br>
+    <label for="password">Password:</label>
+    <input type="text" id="password" name="password"><br><br>
+    <label for="gender">Gender:</label>
+    <input type="radio" id="male" name="gender" value="male">
+    <label for="male">Male</label>
+    <input type="radio" id="female" name="gender" value="female">
+    <label for="female">Female</label>
+    <input type="radio" id="other" name="gender" value="other">
+    <label for="other">Other</label><br><br>
+
+    <input type="submit" value="Submit">
+    <input type="reset" value="Reset">
+  </form>
 
 </body>
+
 </html>
